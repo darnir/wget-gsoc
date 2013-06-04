c2ef3b454a2c67c9573a8875a2d4091d18db1abf
Darshit Shah <darnir@gmail.com>
2013-06-04 22:06:27 +0530

    Convert HTTP Server functions to private and create public accessor
    Signed-off-by: Darshit Shah <darnir@gmail.com>

--- testenv/HTTPServer.py
+++ testenv/HTTPServer.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
+
 #from threading import Thread
 #from socketserver import ThreadingMixIn
+
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
 
-class Handler(BaseHTTPRequestHandler):
+class __Handler(BaseHTTPRequestHandler):
+
+    # The do_* methods define how each of the HTTP Request Methods are handled by this server.
+
     def do_GET(self):
         rootdir = '.'
         filepath = rootdir + self.path
         try:
             f = open(filepath)
-
             self.send_response(200)
             self.send_header("Content-Type", "text/plain")
             self.send_header("Content-Length", os.path.getsize(filepath))
@@ -27,9 +31,12 @@ class Handler(BaseHTTPRequestHandler):
 #class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
 #    pass
 
-def serve_on_port(port):
-    server = HTTPServer(("localhost", port), Handler)
+def __serve_on_port(port):
+    server = HTTPServer(("localhost", port), __Handler)
     server.serve_forever()
 
+# The public interface to this Module. Please do not change interface unless absolutely required.
+def initServer():
+    __serve_on_port(8090)
+
 #Thread(target=serve_on_port, args=[1111]).start()
-serve_on_port(8090)
---
+++ testenv/runTest.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+import HTTPServer
+
+HTTPServer.initServer()
