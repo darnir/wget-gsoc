1f1522c8155bec6956cd440c9dcbe79bb128f38d
Darshit Shah <darnir@gmail.com>
2013-06-07 12:27:53 +0530

    Improve automation. Construct command line and invoke Wget Signed-off-by: Darshit Shah <darnir@gmail.com>

--- testenv/runTest.py
+++ testenv/runTest.py
@@ -6,6 +6,7 @@ import xml.etree.ElementTree as ET
 import sys
 import os
 from multiprocessing import Process
+from subprocess import call
 from time import sleep
 from sys import exit
 
@@ -29,18 +30,35 @@ for TestCase in sys.argv[1:]:
         TestTree = ET.parse(TestCase)
         Root = TestTree.getroot()
         inputFile = []
+        params = []
+        params.append(WgetPath)
+
+        for comm in Root.findall('Option'):
+           params.append(comm.text)
         for filen in Root.findall('InputFile'):
             inputFile.append(filen.text)
+            params.append("localhost:8090/" + filen.get('name'))
+
         start_server(inputFile)
+
+        # Required to so that Wget is not invoked before the Server is initialized
+        sleep(2)
+        retCode = call(params)
+        print(retCode)
+
     else:
        print ("The Test Case File: " + TestCase + " does not exist.")
        exit(1)
 
+# XXX: Check if maybe we can fire the server through the subprocess module and reduce a module dependancy on multiprocessing.
+# XXX: Maybe the server should be spawned through subprocess with an open pipe. The server can then inform
+#      thr parent script upon initialization. Hence removing the use hard sleep(2) statement.
+
 #Replacement Code. Responsibilities.
-# Parse the Test Case File
-# Send the Required Files to the Server.
-# Create the command line to execute
-# Read the return code
-# Decide on Pass or Fail of Test.
-sleep(10)
+# Parse the Test Case File. (In-Progress)
+# Send the Required Files to the Server. (Done)
+# Create the command line to execute. (Done)
+# Read the return code. (Done)
+# Decide on Pass or Fail of Test. (TODO)
+
 stop_server()
