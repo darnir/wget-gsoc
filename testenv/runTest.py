67f0b56e720cbb6366480226c18d6755a85ca0b6
Darshit Shah <darnir@gmail.com>
2013-06-18 17:44:12 +0530

    Fix exit status to work with automake

--- testenv/runTest.py
+++ testenv/runTest.py
@@ -14,6 +14,8 @@ dirn = os.path.dirname (sys.argv[0])
 absp = os.path.abspath (dirn)
 WgetPath = absp + "/../src/wget"
 
+exit_status = 0
+
 # Check for each TestCase file supplied in the arguments.
 # It is however preferable to run multiple tests through an external module
 # that aggregates the results better.
@@ -23,17 +25,19 @@ for TestCase in sys.argv[1:]:
          TestObj = Test (TestCase)
       except TestFailed:
          printer ("RED", "Parse error in Test Case file.")
+         exit_status = 99
          continue
 
       TestObj.start_server ()
 
       TestObj.gen_file_list ()
+      TestObj.parse_special ()
       TestObj.spawn_server ()
       params = TestObj.get_cmd_line (WgetPath)
       parameters = shlex.split (params)
 
       # Required to so that Wget is not invoked before the Server is initialized
-      sleep (2)
+      #sleep (2)
       retCode = call (parameters)
       TestObj.stop_server ()
 
@@ -42,6 +46,7 @@ for TestCase in sys.argv[1:]:
          TestObj.test_downloaded_files ()
       except TestFailed:
          printer ("RED", "Test Failed")
+         exit_status = 100
       else:
          printer ("GREEN", "Test Passed")
          TestObj.endTest ()
@@ -49,4 +54,5 @@ for TestCase in sys.argv[1:]:
    else:
       print ("The Test Case File: " + TestCase + " does not exist.")
 
+exit (exit_status)
 # vim: set ts=8 sw=3 tw=0 et :
