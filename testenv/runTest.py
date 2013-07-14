058252ce317d36a22a8a1e1beb8b0d033e1fdc2b
Darshit Shah <darnir@gmail.com>
2013-07-14 23:18:41 +0530

    Add argparse for commandline parsing

--- testenv/runTest.py
+++ testenv/runTest.py
@@ -3,21 +3,32 @@
 import sys
 import shlex
 import os.path
+import argparse
 from time import sleep
 from subprocess import call
 from ColourTerm import printer
 from TestEnv import Test, TestFailed
 
-# Build the path to local build of Wget
-ABS_PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
-WGET_PATH = os.path.join(ABS_PATH, "..", "src", "wget")
+parser = argparse.ArgumentParser (description="Runner script for Wget Test Environment")
+parser.add_argument ("Files", help="Names of the Test File(s) to execute", nargs='+')
+parser.add_argument ("-s", "--only-server",
+        help="Only spawn Server with given Test configs.", action="store_true")
+parser.add_argument ("-e", "--external", help="Specify path to executable")
+args = parser.parse_args ()
+
+# Build the path to Wget Executable
+if args.external:
+    WGET_PATH = args.external
+else:
+    ABS_PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
+    WGET_PATH = os.path.join(ABS_PATH, "..", "src", "wget")
 
 EXIT_STATUS = 0
 
 # Check for each TestCase file supplied in the arguments.
 # It is however preferable to run multiple tests through an external module
 # that aggregates the results better.
-for TestCase in sys.argv[1:]:
+for TestCase in args.Files:
     if os.path.isfile(TestCase):
         try:
             TestObj = Test(TestCase)
@@ -31,8 +42,10 @@ for TestCase in sys.argv[1:]:
         params = TestObj.get_cmd_line(WGET_PATH)
         parameters = shlex.split(params)
 
-        # Required to so that Wget is not invoked before the Server is initialized
-        #sleep (10000)
+        if args.only_server is True:
+            sleep (10000)
+            TestObj.endTest ()
+            sys.exit (0)
         retCode = call(parameters)
         TestObj.stop_server()
 
